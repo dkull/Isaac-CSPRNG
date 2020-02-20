68282a917b1fdc554eca4d530f50859b55923914
Tanel Liiv <tanel.liiv@defendec.com>
2020-02-20 10:56:03 +0200

    Fix and formatting

--- Isaac.py
+++ Isaac.py
@@ -7,36 +7,36 @@
             y = x.rand(42) # 0<= y <= 41
 
 """
-from math import ceil
+
+mod = 2 ** 32
 
 
-mod = 2**32
-def mix(a,b,c,d,e,f,g,h):
-    a ^= 0xffffffff & b << 11
+def mix(a, b, c, d, e, f, g, h):
+    a ^= 0xFFFFFFFF & b << 11
     d = (d + a) % mod
     b = (b + c) % mod
-    b ^= 0x3fffffff & (c >> 2)
+    b ^= 0x3FFFFFFF & (c >> 2)
     e = (e + b) % mod
     c = (c + d) % mod
-    c ^= 0xffffffff & d << 8
+    c ^= 0xFFFFFFFF & d << 8
     f = (f + c) % mod
     d = (d + e) % mod
-    d ^= (e >> 16)
+    d ^= e >> 16
     g = (g + d) % mod
     e = (e + f) % mod
-    e ^= 0xffffffff & f << 10
+    e ^= 0xFFFFFFFF & f << 10
     h = (h + e) % mod
     f = (f + g) % mod
-    f ^= 0x0fffffff & (g >> 4)
+    f ^= 0x0FFFFFFF & (g >> 4)
     a = (a + f) % mod
     g = (g + h) % mod
-    g ^= 0xffffffff & h << 8
+    g ^= 0xFFFFFFFF & h << 8
     b = (b + g) % mod
     h = (h + a) % mod
-    h ^= 0x007fffff & (a >> 9)
+    h ^= 0x007FFFFF & (a >> 9)
     c = (c + h) % mod
     a = (a + b) % mod
-    return a,b,c,d,e,f,g,h
+    return a, b, c, d, e, f, g, h
 
 
 class Isaac(object):
@@ -50,7 +50,7 @@ class Isaac(object):
 
         self.__randinit__(True)
 
-    def rand(self, mod=2**32):
+    def rand(self, mod=2 ** 32):
         if self.randcnt == 256:
             self.__isaac__()
             self.randcnt = 0
@@ -61,20 +61,20 @@ class Isaac(object):
     def __isaac__(self):
         self.cc += 1
         self.bb += self.cc
-        self.bb &= 0xffffffff
+        self.bb &= 0xFFFFFFFF
 
         for i in range(256):
-            x= self.mm[i]
+            x = self.mm[i]
             switch = i % 4
             xorwith = None
             if switch == 0:
-                 xorwith = (self.aa << 13) % mod
+                xorwith = (self.aa << 13) % mod
             elif switch == 1:
-                 xorwith = self.aa >> 6
+                xorwith = self.aa >> 6
             elif switch == 2:
-                 xorwith = (self.aa << 2) % mod
+                xorwith = (self.aa << 2) % mod
             elif switch == 3:
-                 xorwith = self.aa >> 16
+                xorwith = self.aa >> 16
             else:
                 raise Exception("math is broken")
             self.aa = self.aa ^ xorwith
@@ -83,11 +83,11 @@ class Isaac(object):
             self.randrsl[i] = self.bb = (self.mm[(y >> 10) % 256] + x) % mod
 
     def __randinit__(self, flag):
-        a = b = c = d = e = f = g = h = 0x7e3779b9
+        a = b = c = d = e = f = g = h = 0x9E3779B9
         self.aa = self.bb = self.cc = 0
 
         for x in range(4):
-            a,b,c,d,e,f,g,h = mix(a,b,c,d,e,f,g,h)
+            a, b, c, d, e, f, g, h = mix(a, b, c, d, e, f, g, h)
 
         i = 0
         while i < 256:
@@ -101,8 +101,8 @@ class Isaac(object):
                 g = (g + self.randrsl[i + 6]) % mod
                 h = (h + self.randrsl[i + 7]) % mod
 
-            a,b,c,d,e,f,g,h = mix(a,b,c,d,e,f,g,h)
-            self.mm[i:i+7+1] = a,b,c,d,e,f,g,h
+            a, b, c, d, e, f, g, h = mix(a, b, c, d, e, f, g, h)
+            self.mm[i : i + 7 + 1] = a, b, c, d, e, f, g, h
             i += 8
 
         if flag:
@@ -116,40 +116,41 @@ class Isaac(object):
                 f = (f + self.mm[i + 5]) % mod
                 g = (g + self.mm[i + 6]) % mod
                 h = (h + self.mm[i + 7]) % mod
-                a ^= 0xffffffff & b << 11
+                a ^= 0xFFFFFFFF & b << 11
                 d = (d + a) % mod
                 b = (b + c) % mod
-                b ^= 0x3fffffff & (c >> 2)
+                b ^= 0x3FFFFFFF & (c >> 2)
                 e = (e + b) % mod
                 c = (c + d) % mod
-                c ^= 0xffffffff & d << 8
+                c ^= 0xFFFFFFFF & d << 8
                 f = (f + c) % mod
                 d = (d + e) % mod
-                d ^= (e >> 16)
+                d ^= e >> 16
                 g = (g + d) % mod
                 e = (e + f) % mod
-                e ^= 0xffffffff & f << 10
+                e ^= 0xFFFFFFFF & f << 10
                 h = (h + e) % mod
                 f = (f + g) % mod
-                f ^= 0x0fffffff & (g >> 4)
+                f ^= 0x0FFFFFFF & (g >> 4)
                 a = (a + f) % mod
                 g = (g + h) % mod
-                g ^= 0xffffffff & h << 8
+                g ^= 0xFFFFFFFF & h << 8
                 b = (b + g) % mod
                 h = (h + a) % mod
-                h ^= 0x007fffff & (a >> 9)
+                h ^= 0x007FFFFF & (a >> 9)
                 c = (c + h) % mod
                 a = (a + b) % mod
-                self.mm[i:i+7+1] = a,b,c,d,e,f,g,h
+                self.mm[i : i + 7 + 1] = a, b, c, d, e, f, g, h
                 i += 8
         self.__isaac__()
         self.randcnt = 256
 
+
 if __name__ == "__main__":
     x = Isaac()
     for i in range(512):
-        res = x.rand(2**64)
+        res = x.rand(2 ** 64)
         if i > 0 and i % 8 == 0:
             print()
-        print("{:08x}".format(res), end='')
+        print("{:08x}".format(res), end="")
     print()
